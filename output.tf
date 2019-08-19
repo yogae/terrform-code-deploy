6d60b9909f7dc59fab85dc497118cf3d80b1c985
kim jihyun <>
2019-08-20 02:37:45 +0900

    fix

--- output.tf
+++ output.tf
@@ -3,5 +3,5 @@ output "codedeploy_app_name" {
 }
 
 output "codedeploy_deployment_group_name" {
-  value = "${module.code_deploy.codedeploy_deployment_group_name}"
+  value = "${var.deployment_group_name}"
 }
